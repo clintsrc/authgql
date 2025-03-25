4b7a79007ef0c87bee8319850a5543fd220311e4
Clint Jones <clinton.alan.jones@gmail.com>
2025-03-24 21:14:45 -0700

    fixed typescript issues

---
+++ server/src/models/User.ts
@@ -0,0 +1,67 @@
+/*
+ * User model
+ *
+ * Defines interfaces, schemas, virtual functions, getters and setters
+ * Compiles the User model from the schema
+ *
+ * Each user's savedBooks is a subdocument embedded directly inside their UserDocument
+ * for a one-to-many relationship
+ *
+ */
+
+import { Schema, model, type Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+export interface UserDocument extends Document {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+  isCorrectPassword(password: string): Promise<boolean>;
+  bookCount: number;
+}
+
+const userSchema = new Schema<UserDocument>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// hash user password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = model<UserDocument>("User", userSchema);
+
+export default User;
---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,46 @@
+/*
+ * GraphQL API structure
+ *
+ * Defines the structure of the GraphQL API using Schema Definition Language (SDL).
+ * Specifies data types, queries, and mutations for interacting with the backend.
+ *
+ */
+
+const typeDefs = `
+  ##############  types
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+
+  input UserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  ##############  Queries
+  type Query {
+    user(username: String!): User
+    me: User
+  }
+
+  ##############  Mutations
+  
+  # Add a new user (signup)
+  addUser(input: UserInput!): Auth
+
+  # Login user and return JWT token (signin)
+  loginUser(email: String!, password: String!): Auth
+`;
+
+export default typeDefs;

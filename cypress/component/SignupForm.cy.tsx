77b487d49fcbb096339250773b381c0cf2661c5b
Clint Jones <clinton.alan.jones@gmail.com>
2025-03-26 17:39:06 -0700

    More cypress fixes

---
+++ cypress/component/SignupForm.cy.tsx
@@ -0,0 +1,9 @@
+import SignupForm from "../../client/src/components/SignupForm";
+import { mount } from "cypress/react";
+
+describe("<SignupForm />", () => {
+  it("TODO", () => {
+    mount(<SignupForm handleModalClose={() => console.log("Modal closed")} />);
+    console.log("TODO");
+  });
+});

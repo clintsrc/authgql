c4679e55fa35a1507688441882aa6b5500fd17b4
Clint Jones <clinton.alan.jones@gmail.com>
2025-03-28 11:41:40 -0700

    Rudimentary cypress tests that pass

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "cypress";
-import viteConfig from './vite.config.js';
+import viteConfig from "./vite.config";
 
 export default defineConfig({
   component: {
     port: 5173,
     devServer: {
       framework: "react",
-      bundler: 'vite',
+      bundler: "vite",
       viteConfig,
     },
   },
@@ -17,5 +17,5 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  }
+  },
 });
--- cypress/component/SignupForm.cy.tsx
+++ cypress/component/SignupForm.cy.tsx
@@ -1,9 +1,43 @@
-import SignupForm from "../../client/src/components/SignupForm";
+import { MockedProvider } from "@apollo/client/testing";
 import { mount } from "cypress/react";
+import SignupForm from "../../client/src/components/SignupForm";
+import { LOGIN_USER } from "../../client/src/utils/mutations";
+
+const mocks = [
+  {
+    request: {
+      query: LOGIN_USER,
+      variables: {
+        username: "test@test.com",
+        password: "test",
+      },
+    },
+    result: {
+      data: {
+        login: {
+          __typename: "LoginResponse",
+          token: "mocked-token",
+          user: {
+            __typename: "User",
+            id: "1",
+            email: "test@test.com",
+          },
+        },
+      },
+    },
+  },
+];
 
 describe("<SignupForm />", () => {
-  it("TODO", () => {
-    mount(<SignupForm handleModalClose={() => console.log("Modal closed")} />);
-    console.log("TODO");
+  it("should submit the form and get a successful response", () => {
+    mount(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <SignupForm handleModalClose={() => console.log("Modal closed")} />
+      </MockedProvider>
+    );
+
+    cy.get("input[name='email']").type("test@test.com");
+    cy.get("input[name='password']").type("test");
+    cy.get("button[type='submit']").should('be.visible');
   });
 });
